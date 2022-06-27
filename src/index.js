d83e3e40f8a8fa86f135375fb57ba8fcb9edbb7e
Tim <hoilonuzero@gmail.com>
2022-06-28 02:43:13 +0300

    feat: MAKE IT ?

--- src/index.js
+++ src/index.js
@@ -1,3 +1,37 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable (n) {
+    let b = String(Math.abs(n));
+    let number = 
+    ['zero','one','two','three','four','five','six','seven','eigth','nine','ten',
+    'eleven','twlve','thirteen','fourteen','fifteen','sixteen','seventeen',
+    'eighteen','nineteen','twenty'];
+    number[30]='thirty';
+    number[40]='forty';
+    number[50]='fifty';
+    number[60]='sixty';
+    number[70]='seventy';
+    number[80]='eighty';
+    number[90]='ninety';
+    number[100]='hundred';
+    let rez='';
+    
+    if (b.length == 3) {
+        rez += number[b[0]];
+        rez += ' ' + number[100]
+        if (Number(b[1] + b[2]) < 21)
+            rez += ' ' + number[Number(b[1] + b[2])];
+        else {
+            rez += ' ' + number[Number(b[1] + '0')];
+            rez += ' ' + number[Number(b[2])];
+        }
+    }
+    else if (b.length == 2){
+        if (Number(b[0] + b[1]) < 21)
+            rez += ' ' + number[Number(b[0] + b[1])];
+        else {
+            rez += ' ' + number[Number(b[0] + '0')];
+            rez += ' ' + number[Number(b[1])];
+        }
+    }
+    else    rez +=number[Number(b[0])];
+    return rez;
 }
